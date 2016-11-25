6245f1f282cd9f170ba2427d0aabaa27da6e9f55
helenemartin <helenemartin13@hotmail.com>
2016-11-25 16:18:48 +0000

    add script and css styling

---
+++ js/script.js
@@ -0,0 +1,43 @@
+function $snow() {
+  var snow = $('<div id="snow"/>').css({
+      'position': 'absolute',
+      'top': '-50px'
+    }).html(' &#x2022;'),
+    animationHeight = $('.snow-container').height(),
+    animationWidth = $(document).width(),
+    snowCount = 500;
+
+  setInterval(function() {
+
+    var startPositionLeft = Math.random() * animationWidth,
+      startPositionTop = 0,
+      startOpacity = 1,
+      duration = Math.random() * animationHeight + 7000,
+      snowCol = '#fff',
+      endPositionLeft = startPositionLeft - 100 + Math.random() * 200,
+      endPositionTop = animationHeight + 100;
+
+    snow.clone().appendTo('.snow-container').css({
+      left: startPositionLeft,
+      top: startPositionTop,
+      opacity: startOpacity,
+      'color': snowCol
+
+    }).animate({
+        opacity: 0,
+        top: endPositionTop,
+        left: endPositionLeft,
+      },
+      duration,
+      'linear',
+
+      function() {
+        $(this).remove();
+      });
+  }, snowCount);
+
+}
+
+$(document).ready(function() { 
+  $snow();
+});
